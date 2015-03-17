cc432a4f9e1e3ac8cd03b9631cd6101b5a5961bd
Shirai <shirai.kenta11@gmail.com>
2015-03-17 17:41:18 +0900

    アホみたいな間違いを修正

--- antikaruhage.rb
+++ antikaruhage.rb
@@ -7,14 +7,10 @@ Plugin.create(:antikaruhage) do
   on_appear do |ms|
     ms.each do |m|
       if m.user.idname == "karubabu" and m[:created] > DEFINED_TIME and !m.retweet?
-        exptmp = m.to_s
-        # mentionを弾く
-        if exptmp !=~ /@[a-zA-Z0-9_]*/
-          # 空の()を弾く
-          exptmp = exptmp.gsub(/[\(（][\)）]/,'')
-          exptmp = exptmp.gsub(/(https?|ftp):\/\/[\/A-Za-z0-9\.]*/,'')
-          if exptmp =~ /^よし|しよう$|します$|では$|するね$|したい$|てみよう$|いいね|してね$|ようね$|ますね$|/ and m[:created] > DEFINED_TIME and !m.retweet?
-            randtmp = rand(8) + 1
+        if m.body =~ /^よし|しよう$|します$|では$|するね$|したい$|てみよう$|いいね|してね$|ようね$|ますね$/
+          # mentionを弾く
+          if m.body !=~ /@[a-zA-Z0-9_]*/
+            randtmp = rand(7) + 1
             case randtmp
             when 1 then
               reply = "だめ"
@@ -25,13 +21,11 @@ Plugin.create(:antikaruhage) do
             when 4 then
               reply = "ダメです"
             when 5 then
-              reply = "だめで～す"
+              reply = "だめ\n"
             when 6 then
-              reply = "だめでーす"
+              reply = "だめ\r"
             when 7 then
-              reply = "ダメで～す"
-            when 8 then
-              reply = "ダメでーす"
+              reply = "だめ\r\n"
             end
             Service.primary.post(:message => "#{"@" + m.user.idname + " " + reply}", :replyto => m)
           end
