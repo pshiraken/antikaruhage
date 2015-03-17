da5ee968ba2a113543070a3270a67eb033d7886a
Shirai <shirai.kenta11@gmail.com>
2015-03-17 16:04:38 +0900

    がんばった

--- antikaruhage.rb
+++ antikaruhage.rb
@@ -8,32 +8,33 @@ Plugin.create(:antikaruhage) do
     ms.each do |m|
       if m.user.idname == "karubabu" and m[:created] > DEFINED_TIME and !m.retweet?
         exptmp = m.to_s
-        # @screen_nameを弾く
-        exptmp = exptmp.gsub(/@[a-zA-Z0-9_]*/,'')
-        # 空の()を弾く
-        exptmp = exptmp.gsub(/[\(（][\)）]/,'')
-        exptmp = exptmp.gsub(/(https?|ftp):\/\/[\/A-Za-z0-9\.]*/,'')
-        if exptmp =~ /^よし|しよう$|します$|では$|するね$|したい$|てみよう$|いいね/ and m[:created] > DEFINED_TIME and !m.retweet?
-          randtmp = rand(8) + 1
-          case randtmp
-          when 1 then
-            reply = "だめ"
-          when 2 then
-            reply = "だめです"
-          when 3 then
-            reply = "ダメ"
-          when 4 then
-            reply = "ダメです"
-          when 5 then
-            reply = "だめで～す"
-          when 6 then
-            reply = "だめでーす"
-          when 7 then
-            reply = "ダメで～す"
-          when 8 then
-            reply = "ダメでーす"
+        # mentionを弾く
+        if exptmp !=~ /@[a-zA-Z0-9_]*/
+          # 空の()を弾く
+          exptmp = exptmp.gsub(/[\(（][\)）]/,'')
+          exptmp = exptmp.gsub(/(https?|ftp):\/\/[\/A-Za-z0-9\.]*/,'')
+          if exptmp =~ /^よし|しよう$|します$|では$|するね$|したい$|てみよう$|いいね|してね$|ようね$|ますね$|/ and m[:created] > DEFINED_TIME and !m.retweet?
+            randtmp = rand(8) + 1
+            case randtmp
+            when 1 then
+              reply = "だめ"
+            when 2 then
+              reply = "だめです"
+            when 3 then
+              reply = "ダメ"
+            when 4 then
+              reply = "ダメです"
+            when 5 then
+              reply = "だめで～す"
+            when 6 then
+              reply = "だめでーす"
+            when 7 then
+              reply = "ダメで～す"
+            when 8 then
+              reply = "ダメでーす"
+            end
+            Service.primary.post(:message => "#{"@" + m.user.idname + " " + reply}", :replyto => m)
           end
-          Service.primary.post(:message => "#{"@" + m.user.idname + " " + reply}", :replyto => m)
         end
       end
     end
